5ab141c55ac178efce222d3a5bf2740ba52a7192
hacker531 <hacker531@bogusemail.k3kabs.com>
2018-07-08 23:36:28 -0500

    added reseting game once full

--- sketch.js
+++ sketch.js
@@ -16,9 +16,24 @@ function setup() {
 
 function draw() {
   background('#BF9C7D');
+  if(posAvailable.length == 0) {
+    posAvailable = [];
+    tiles = [];
+    for(let i = 0; i < 4; i++) {
+      for(let j = 0; j < 4; j++) {
+        posAvailable.push([i,j]);
+      }
+    }
+    for(let i = 0; i < 3; i++) {
+      var t = new Tile(this.posAvailable);
+      tiles.push(t);
+    }
+  }
+
   for(var t = 0; t < tiles.length; t++) {
     tiles[t].show();
   }
+  
   for(var g = 0; g < posAvailable.length; g++) {
     fill('#C9B39F');
     let xPos = (posAvailable[g][0] * 100) + 5;
@@ -31,40 +46,47 @@ function draw() {
 
   function keyPressed() {
     if(keyCode === RIGHT_ARROW) {
-      for(var i = 2; i >=0; i--) {
+      for(var i = 3; i >=0; i--) {
         for(var t = 0; t < tiles.length; t++) {
           if(tiles[t].position[0] == i) {
             tiles[t].moveRight(tiles, posAvailable);
           }
         }
       }
+      var t = new Tile(this.posAvailable);
+      tiles.push(t);
     }
     if(keyCode === LEFT_ARROW) {
-      for(var i = 1; i <=3 ; i++) {
+      for(var i = 0; i <=3 ; i++) {
         for(var t = 0; t < tiles.length; t++) {
           if(tiles[t].position[0] == i) {
             tiles[t].moveLeft(tiles, posAvailable);
           }
         }
       }
+      var t = new Tile(this.posAvailable);
+      tiles.push(t);
     }
     if(keyCode === UP_ARROW) {
-      for(var i = 1; i <= 3; i++) {
+      for(var i = 0; i <= 3; i++) {
         for(var t = 0; t < tiles.length; t++) {
           if(tiles[t].position[1] == i) {
             tiles[t].moveUp(tiles, posAvailable);
           }
         }
       }
+      var t = new Tile(this.posAvailable);
+      tiles.push(t);
     }
     if(keyCode === DOWN_ARROW) {
-      for(var i = 2; i >=0; i--) {
+      for(var i = 3; i >=0; i--) {
         for(var t = 0; t < tiles.length; t++) {
           if(tiles[t].position[1] == i) {
             tiles[t].moveDown(tiles, posAvailable);
           }
         }
       }
+      var t = new Tile(this.posAvailable);
+      tiles.push(t);
     }
-
   }

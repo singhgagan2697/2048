e8889bb5bdfee17ee3ac77f84983932ca7499ffc
hacker531 <hacker531@bogusemail.k3kabs.com>
2018-07-08 23:21:54 -0500

    added moving

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 var tiles = [];
-var grid = [];
+var posAvailable = [];
 
 function setup() {
   createCanvas(400, 400);
   for(let i = 0; i < 4; i++) {
     for(let j = 0; j < 4; j++) {
-      grid.push([i,j]);
+      posAvailable.push([i,j]);
     }
   }
   for(let i = 0; i < 3; i++) {
-    tiles.push(new Tile(this.grid));
+    var t = new Tile(this.posAvailable);
+    tiles.push(t);
   }
 }
 
@@ -18,11 +19,52 @@ function draw() {
   for(var t = 0; t < tiles.length; t++) {
     tiles[t].show();
   }
-  for(var g = 0; g < grid.length; g++) {
+  for(var g = 0; g < posAvailable.length; g++) {
     fill('#C9B39F');
-    let xPos = (grid[g][0] * 100) + 5;
-    let yPos = (grid[g][1] * 100) + 5;
+    let xPos = (posAvailable[g][0] * 100) + 5;
+    let yPos = (posAvailable[g][1] * 100) + 5;
     noStroke();
     rect(xPos, yPos, 90, 90, 5);
   }
+
 }
+
+  function keyPressed() {
+    if(keyCode === RIGHT_ARROW) {
+      for(var i = 2; i >=0; i--) {
+        for(var t = 0; t < tiles.length; t++) {
+          if(tiles[t].position[0] == i) {
+            tiles[t].moveRight(tiles, posAvailable);
+          }
+        }
+      }
+    }
+    if(keyCode === LEFT_ARROW) {
+      for(var i = 1; i <=3 ; i++) {
+        for(var t = 0; t < tiles.length; t++) {
+          if(tiles[t].position[0] == i) {
+            tiles[t].moveLeft(tiles, posAvailable);
+          }
+        }
+      }
+    }
+    if(keyCode === UP_ARROW) {
+      for(var i = 1; i <= 3; i++) {
+        for(var t = 0; t < tiles.length; t++) {
+          if(tiles[t].position[1] == i) {
+            tiles[t].moveUp(tiles, posAvailable);
+          }
+        }
+      }
+    }
+    if(keyCode === DOWN_ARROW) {
+      for(var i = 2; i >=0; i--) {
+        for(var t = 0; t < tiles.length; t++) {
+          if(tiles[t].position[1] == i) {
+            tiles[t].moveDown(tiles, posAvailable);
+          }
+        }
+      }
+    }
+
+  }
--- tile.js
+++ tile.js
@@ -1,28 +1,179 @@
-function Tile(grid) {
+function Tile(posAvailable) {
 
   var value;
   var xPos;
   var yPos;
-  var area = 90;
+  var position;
+  var area;
+  this.area = 90;
 
   this.value = (random() <= 0.9 ? 2 : 4);
 
-  let index = Math.floor(Math.random() * grid.length);
-  let position = grid[index];
-  grid.splice(index, 1);
+  let index = Math.floor(Math.random() * posAvailable.length);
+  this.position = posAvailable[index];
 
-  this.xPos = (position[0] * 100) + (100-area)/2;
-  this.yPos = (position[1] * 100) + (100-area)/2;
+  posAvailable.splice(index, 1);
+
+  this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+  this.yPos = (this.position[1] * 100) + (100-this.area)/2;
 
 
   this.show = function(){
     fill('#C9B39F');
     noStroke();
-    rect(this.xPos, this.yPos, area, area, 5);
-    fill(255);
+    rect(this.xPos, this.yPos, this.area, this.area, 5);
+    if(this.value > 4) {
+      fill(255);
+    }
+    else {
+      fill('#4B3B2D');
+    }
     textSize(40);
     textAlign(CENTER);
     text(this.value, this.xPos + 45, this.yPos + 55);
   }
 
+  this.moveRight = function(tiles, posAvailable) {
+    while(this.position[0] < 3) {
+      var posToFind = [this.position[0] + 1, this.position[1]];
+      for(var t = 0; t < tiles.length; t++) {
+        if(tiles[t].position[0] == posToFind[0] && tiles[t].position[1] == posToFind[1]) {
+          if(tiles[t].value == this.value) {
+            this.value *= 2;
+            posAvailable.push(this.position);
+            this.position = posToFind;
+            this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+            this.yPos = (this.position[1] * 100) + (100-this.area)/2;
+            tiles.splice(t, 1);
+            return;
+          }
+          else {
+            return;
+          }
+        }
+      }
+      var delIndex = -1;
+      for(var i = 0; i < posAvailable.length; i++) {
+        if(posAvailable[i][0] == posToFind[0] && posAvailable[i][1] == posToFind[1]) {
+          delIndex = i;
+          break;
+        }
+      }
+      posAvailable.splice(delIndex, 1);
+
+      posAvailable.push(this.position);
+      this.position = posToFind;
+      this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+      this.yPos = (this.position[1] * 100) + (100-this.area)/2;
+
+    }
+  }
+
+  this.moveLeft = function(tiles, posAvailable) {
+    while(this.position[0] > 0) {
+      var posToFind = [this.position[0] - 1, this.position[1]];
+      for(var t = 0; t < tiles.length; t++) {
+        if(tiles[t].position[0] == posToFind[0] && tiles[t].position[1] == posToFind[1]) {
+          if(tiles[t].value == this.value) {
+            this.value *= 2;
+            posAvailable.push(this.position);
+            this.position = posToFind;
+            this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+            this.yPos = (this.position[1] * 100) + (100-this.area)/2;
+            tiles.splice(t, 1);
+            return;
+          }
+          else {
+            return;
+          }
+        }
+      }
+      var delIndex = -1;
+      for(var i = 0; i < posAvailable.length; i++) {
+        if(posAvailable[i][0] == posToFind[0] && posAvailable[i][1] == posToFind[1]) {
+          delIndex = i;
+          break;
+        }
+      }
+      posAvailable.splice(delIndex, 1);
+
+      posAvailable.push(this.position);
+      this.position = posToFind;
+      this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+      this.yPos = (this.position[1] * 100) + (100-this.area)/2;
+
+    }
+  }
+
+  this.moveUp = function(tiles, posAvailable) {
+    while(this.position[1] > 0) {
+      var posToFind = [this.position[0], this.position[1] - 1];
+      for(var t = 0; t < tiles.length; t++) {
+        if(tiles[t].position[0] == posToFind[0] && tiles[t].position[1] == posToFind[1]) {
+          if(tiles[t].value == this.value) {
+            this.value *= 2;
+            posAvailable.push(this.position);
+            this.position = posToFind;
+            this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+            this.yPos = (this.position[1] * 100) + (100-this.area)/2;
+            tiles.splice(t, 1);
+            return;
+          }
+          else {
+            return;
+          }
+        }
+      }
+      var delIndex = -1;
+      for(var i = 0; i < posAvailable.length; i++) {
+        if(posAvailable[i][0] == posToFind[0] && posAvailable[i][1] == posToFind[1]) {
+          delIndex = i;
+          break;
+        }
+      }
+      posAvailable.splice(delIndex, 1);
+
+      posAvailable.push(this.position);
+      this.position = posToFind;
+      this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+      this.yPos = (this.position[1] * 100) + (100-this.area)/2;
+
+    }
+  }
+
+  this.moveDown = function(tiles, posAvailable) {
+    while(this.position[1] < 3) {
+      var posToFind = [this.position[0], this.position[1] + 1];
+      for(var t = 0; t < tiles.length; t++) {
+        if(tiles[t].position[0] == posToFind[0] && tiles[t].position[1] == posToFind[1]) {
+          if(tiles[t].value == this.value) {
+            this.value *= 2;
+            posAvailable.push(this.position);
+            this.position = posToFind;
+            this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+            this.yPos = (this.position[1] * 100) + (100-this.area)/2;
+            tiles.splice(t, 1);
+            return;
+          }
+          else {
+            return;
+          }
+        }
+      }
+      var delIndex = -1;
+      for(var i = 0; i < posAvailable.length; i++) {
+        if(posAvailable[i][0] == posToFind[0] && posAvailable[i][1] == posToFind[1]) {
+          delIndex = i;
+          break;
+        }
+      }
+      posAvailable.splice(delIndex, 1);
+
+      posAvailable.push(this.position);
+      this.position = posToFind;
+      this.xPos = (this.position[0] * 100) + (100-this.area)/2;
+      this.yPos = (this.position[1] * 100) + (100-this.area)/2;
+
+    }
+  }
 }
